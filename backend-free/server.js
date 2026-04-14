8f589963c6280647a79f7bc9c0f672fd07e345ea
Simran <singhsimran0227@gmail.com>
2026-04-14 13:57:28 +0000

    fix: added express

--- backend-free/server.js
+++ backend-free/server.js
@@ -28,6 +28,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // good to have too
 
 
 // Health check
